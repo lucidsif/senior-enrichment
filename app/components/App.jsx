99204974ab6bdf60a7ac9bf6d095b59ff34b0ea7
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-08-27 14:20:47 -0400

    setup react router and changed Main.jsx to App.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Home from './Home';
 import DarkNavBar from './Navbar';
 
-export default function Main(props) {
+export default function App(props) {
     return (
         <div>
             <DarkNavBar />
-            <Home />
+            <Route exact path='/' component={Home} />
         </div>
     )
 }
--- app/main.jsx
+++ app/main.jsx
@@ -2,14 +2,17 @@
 import React from 'react'
 import {render} from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
 
 import store from './store'
 //import Root from './components/Root'
-import Main from './components/Main';
+import App from './components/App';
 
 render (
   <Provider store={store}>
-      <Main />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </Provider>,
   document.getElementById('main')
 )
