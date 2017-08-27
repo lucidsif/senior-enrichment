fc6e36bfd1465f0ba8f10d2a13686d210f3bcb23
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-08-27 14:24:29 -0400

    fixed up navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 export default function DarkNavbar() {
     return (
         <Navbar inverse collapseOnSelect>
             <Navbar.Header>
                 <Navbar.Brand>
-                    <a href="#">React-Bootstrap</a>
+                    <a href="#">Directory of Galactic Degree Mills</a>
                 </Navbar.Brand>
                 <Navbar.Toggle />
             </Navbar.Header>
             <Navbar.Collapse>
-                <Nav>
-                    <NavItem eventKey={1} href="#">Link</NavItem>
-                    <NavItem eventKey={2} href="#">Link</NavItem>
-                    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Action</MenuItem>
-                        <MenuItem eventKey={3.2}>Another action</MenuItem>
-                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                        <MenuItem divider />
-                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                    </NavDropdown>
-                </Nav>
                 <Nav pullRight>
-                    <NavItem eventKey={1} href="#">Link Right</NavItem>
-                    <NavItem eventKey={2} href="#">Link Right</NavItem>
+                    <NavItem eventKey={1} href="#">Home</NavItem>
+                    <NavItem eventKey={2} href="#">Studs</NavItem>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
