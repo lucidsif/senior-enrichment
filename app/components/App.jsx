2b425dc8ee23b6058496b3f3308d1d7ba40a8214
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-08-27 15:04:44 -0400

    added routes for campuses

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
 import DarkNavBar from './Navbar';
+import PlanetaryCampus from "./PlanetaryCampus";
 
 export default function App(props) {
     return (
         <div>
             <DarkNavBar />
             <Route exact path='/' component={Home} />
+            <Route exact path='/campus/:id' component={PlanetaryCampus} />
         </div>
     )
 }
