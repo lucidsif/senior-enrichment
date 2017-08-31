be61bb4cd3a2c1a638a20a6aa49fda0c55943bba
Tawsif Ahmed <Sif@Tawsifs-MacBook-Pro.local>
2017-08-31 09:56:07 -0400

    fixed tests

--- app/tests/redux.spec.js
+++ app/tests/redux.spec.js
@@ -1,8 +1,8 @@
 import {expect} from 'chai';
 import {createStore} from 'redux';
 import rootReducer from '../reducers';
-import {GET_CAMPUSES, GET_CAMPUS, SELECT_CAMPUS, GET_STUDENTS, GET_STUDENT, ADD_STUDENT} from '../action-creators/actions';
-import {getCampuses, getCampus, selectCampus, getStudents, getStudent, addStudent} from "../action-creators/actions";
+import {GET_CAMPUSES, GET_STUDENTS} from '../action-creators/actions';
+import {getCampuses, getStudents} from "../action-creators/actions";
 
 const testCampusesArr = [{
     id: 2,
@@ -49,24 +49,6 @@ const testStudents = [{
         })
     });
 
-    describe('getCampus', () => {
-        it('returns properly formatted action', () => {
-            expect(getCampus(testCampusesArr[0])).to.be.deep.equal({
-                type: 'GET_CAMPUS',
-                campus: testCampusesArr[0]
-            })
-        })
-    });
-
-    describe('selectCampus', () => {
-        it('returns properly formated action', () => {
-            expect(selectCampus(testCampusesArr[0])).to.be.deep.equal({
-                type: 'SELECT_CAMPUS',
-                selectedCampus: testCampusesArr[0]
-            })
-        })
-    });
-
     describe('getStudents', () => {
         it('returns properly formatted action', () => {
             expect(getStudents(testStudents)).to.be.deep.equal({
@@ -76,33 +58,13 @@ const testStudents = [{
         })
     });
 
-    describe('getStudent', () => {
-        it('returns a properly formatted action', () => {
-            expect(getStudent(testStudents[0])).to.be.deep.equal({
-                type: 'GET_STUDENT',
-                student: testStudents[0]
-            })
-        })
-    })
-
-    describe('addStudent', () => {
-        it('returns properly formatted action', () => {
-            expect(addStudent(testStudents[0])).to.be.deep.equal({
-                type: 'ADD_STUDENT',
-                addedStudent: testStudents[0]
-            });
-        })
-    });
-
 });
 
 describe('Reducer', () => {
 
     const initialState = {
         campuses: [],
-        students: [],
-        addedStudent: {},
-        selectedCampus: {}
+        students: []
     };
     let testStore;
 
@@ -124,8 +86,6 @@ describe('Reducer', () => {
         const nextState = {
             campuses: testCampusesArr,
             students: [],
-            addedStudent: {},
-            selectedCampus: {}
         };
 
         it('sets new campuses from the action creator to campuses', () => {
@@ -133,71 +93,18 @@ describe('Reducer', () => {
         });
     });
 
-    describe('GET_CAMPUS', () => {
-        const nextState = {
-            campuses: [testCampusesArr[0]],
-            students: [],
-            addedStudent: {},
-            selectedCampus: {}
-        };
-
-        it('sets a new campus from the action creator to campuses', () => {
-            expect(rootReducer(undefined, {type: GET_CAMPUS, campus: testCampusesArr[0]})).to.deep.equal(nextState);
-        })
-    });
-
-    describe('SELECT_CAMPUS', () => {
-        const nextState = {
-            campuses: [],
-            students: [],
-            addedStudent: {},
-            selectedCampus: testCampusesArr[0]
-        };
-
-        it('sets a new campus to selectedCampus', () => {
-            expect(rootReducer(undefined, {
-                type: SELECT_CAMPUS,
-                selectedCampus: testCampusesArr[0]
-            })).to.deep.equal(nextState)
-        })
-    });
-
     describe('GET_STUDENTS', () => {
         const nextState = {
             campuses: [],
             students: testStudents,
-            addedStudent: {},
-            selectedCampus: {},
         };
         it('should set students to the students from the action', () => {
             expect(rootReducer(undefined, getStudents(testStudents))).to.be.deep.equal(nextState);
         })
     });
 
-    describe('GET_STUDENT', () => {
-        const nextState = {
-            campuses: [],
-            students: [testStudents[0]],
-            addedStudent: {},
-            selectedCampus: {},
-        };
-        it('should set the student from the action to students', () => {
-            expect(rootReducer(undefined, getStudent(testStudents[0]))).to.be.deep.equal(nextState);
-        })
-    });
 
-    describe('ADD_STUDENT', () => {
-        const nextState = {
-            campuses: [],
-            students: [],
-            addedStudent: {name: 'Kelly'},
-            selectedCampus: {},
-        };
 
-        it('should set the addedstudent from the action to added student', () => {
-            expect(rootReducer(undefined, addStudent({name: 'Kelly'}))).to.be.deep.equal(nextState);
-        })
-    })
 });
 
 // TODO: Fetch campuses and students on app load
