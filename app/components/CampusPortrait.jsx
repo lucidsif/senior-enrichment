8ac357d466db51dad34654999a5ba613f717ad5a
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-09-01 00:41:01 -0400

    finished all user stories

--- app/components/CampusPortrait.jsx
+++ app/components/CampusPortrait.jsx
@@ -4,11 +4,16 @@ import {connect} from 'react-redux';
 import {Jumbotron, ListGroup, ListGroupItem} from 'react-bootstrap';
 import UpdateFormModal from './UpdateFormModal';
 import FormModal from './FormModal';
+import {deleteStudent} from "../action-creators/actions";
 
 export function PlanetaryCampus(props) {
     const id = +props.match.params.id;
     const campus = props.campuses.filter((campus) => campus.id === id)[0];
-    let counter = 0;
+    let index = 0;
+    function handleDelete(id) {
+        const deleteThunk = deleteStudent(id);
+        props.delete(deleteThunk);
+    }
     return (
         <div className="container">
             <Jumbotron>
@@ -19,8 +24,7 @@ export function PlanetaryCampus(props) {
             <ListGroup>
                 {
                     campus.students.map((student) => {
-                        counter++;
-                        return <ListGroupItem key={student.id}>{counter} - <Link to={`/suckers/${student.id}`}>{student.name}</Link></ListGroupItem>
+                        return <ListGroupItem key={student.id}><span onClick={() => handleDelete(student.id)}className="glyphicon glyphicon-trash red-formatted" aria-hidden="true"></span>{++index} - <Link to={`/suckers/${student.id}`}>{student.name}</Link></ListGroupItem>
 
                     })
                 }
@@ -35,7 +39,15 @@ function mapStateToProps(state) {
     }
 }
 
-const EnhancedPlanetaryCampus =  connect(mapStateToProps, null)(PlanetaryCampus);
+function mapDispatchToProps(dispatch) {
+    return {
+        delete: function(thunk) {
+            dispatch(thunk);
+        }
+    }
+}
+
+const EnhancedPlanetaryCampus =  connect(mapStateToProps, mapDispatchToProps)(PlanetaryCampus);
 
 export default EnhancedPlanetaryCampus;
 
